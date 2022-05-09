fee310b5500038cc0d64cdaba426755dc97cd51a
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-05-09 22:13:13 +0600

    Add project
    This python program is a calculator that takes 2 numbers from the user and calculate them based on the operator provided by the user.

---
+++ Project5-Better_Calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input('Enter first number: '))
+operator = input('Enter Operator: ')
+num2 = float(input('Enter second number: '))
+
+print("\n")
+
+if operator == '+':
+    print(f'Result: {num1 + num2}')
+elif operator == '-':
+    print(f'Result: {num1 - num2}')
+elif operator == '*':
+    print(f'Result: {num1 * num2}')
+elif operator == '/':
+    print(f'Result: {num1 / num2}')
+elif operator == '**':
+    print(f'Result: {num1 ** num2}')
+else:
+    print("Invalid Operation!")
